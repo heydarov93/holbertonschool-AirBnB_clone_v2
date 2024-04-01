359a844f5f89a73a19574264f839b6da096f2c85
heydarov93 <heyderov.yashar@mail.ru>
2024-04-01 21:40:56 +0400

    class attributes of base model mapped to db table

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,20 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, String
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
 
         try:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
@@ -22,7 +29,6 @@ class BaseModel:
             kwargs['created_at'] = datetime.now()
             kwargs['id'] = str(uuid.uuid4())
             self.__dict__.update(kwargs)
-            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -33,6 +39,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -43,4 +50,14 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
+
         return dictionary
+
+    def delete(self):
+        """deletes the current instance from the storage"""
+        from models import storage
+
+        storage.delete(self)

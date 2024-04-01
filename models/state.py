4c93485e6c2fe079c668b710643e515a185132cc
heydarov93 <heyderov.yashar@mail.ru>
2024-04-01 21:41:59 +0400

    mapped to table and established relationship

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
+
+Base = declarative_base()
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    # for DBStorage
+    # one to many
+    if environ("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship(
+                                "City",
+                                back_populates="states",
+                                cascade="all, delete"
+                                )
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+
+            all_cities = istorage.all(City)
+            cities_of_state = [v if v.state_id == self.id for k, v
+                               in all_cities.items()]
+            return cities_of_state

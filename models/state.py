8f629349beffb71f5d91bdc532e8daef368edaed
heydarov93 <heyderov.yashar@mail.ru>
2024-04-03 00:58:47 +0400

    unnecessary import lines removed

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv

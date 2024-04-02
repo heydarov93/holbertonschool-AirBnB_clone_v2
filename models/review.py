31c47309f6c63e443be297f3bf4fc003584fc81f
heydarov93 <heyderov.yashar@mail.ru>
2024-04-03 03:12:45 +0400

    's' added to the end of the 'places' in Foreign Key (places.id)

--- models/review.py
+++ models/review.py
@@ -8,6 +8,6 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
 
-    place_id = Column(String(60), nullable=False, ForeignKey("place.id"))
+    place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
     user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
     text = Column(String(1024), nullable=False)

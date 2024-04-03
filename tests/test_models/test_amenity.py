5b892afaa572f95050f0a5af692ae33912f0f1e9
aelitaa <gunel.sultanova@outlook.com>
2024-04-03 15:20:45 +0400

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """ """
+import os
+
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
-
+class TestAmenity(test_basemodel):
+    """Represents the tests for the Amenity model."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        """Tests the type of name."""
+        new = self.value(name="Test Amenity")
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
+

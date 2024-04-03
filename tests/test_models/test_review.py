5e468ed4f09751c7c660a9dc63c9b0c94139a94f
aelitaa <gunel.sultanova@outlook.com>
2024-04-03 15:18:04 +0400

    review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
 """ """
+import os
+
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
-class test_review(test_basemodel):
-    """ """
-
+class TestReview(test_basemodel):
+    """Represents the tests for the Review model."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        """Tests the type of place_id."""
+        new = self.value(place_id="test_place_id")
+        self.assertEqual(
+            type(new.place_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        """Tests the type of user_id."""
+        new = self.value(user_id="test_user_id")
+        self.assertEqual(
+            type(new.user_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        """Tests the type of text."""
+        new = self.value(text="This is a review text")
+        self.assertEqual(
+            type(new.text),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
+

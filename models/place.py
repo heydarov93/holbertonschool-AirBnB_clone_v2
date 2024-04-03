1829b5d7ab255cc0065a83bb17fa5eee36e94a7c
heydarov93 <heyderov.yashar@mail.ru>
2024-04-03 10:06:25 +0400

    create association table statement moved into Place class

--- models/place.py
+++ models/place.py
@@ -6,23 +6,6 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-    place_amenity = Table("place_amenity", Base.metadata,
-                          Column(
-                                 "place_id",
-                                 String(60),
-                                 ForeignKey("places.id")
-                                 primary_key=True,
-                                 nullable=False
-                                 ),
-                          Column(
-                                 "amenity_id",
-                                 String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True,
-                                 nullable=False
-                                 ))
-
-
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
@@ -38,6 +21,22 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    place_amenity = Table("place_amenity", Base.metadata,
+                          Column(
+                                 "place_id",
+                                 String(60),
+                                 ForeignKey("places.id")
+                                 primary_key=True,
+                                 nullable=False
+                                 ),
+                          Column(
+                                 "amenity_id",
+                                 String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True,
+                                 nullable=False
+                                 ))
 
     # for DBStorage
     # one to many

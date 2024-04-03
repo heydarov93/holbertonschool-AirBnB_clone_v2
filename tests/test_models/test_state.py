4767fc0be11159f2b6a45942b8fd48598eae0e4d
aelitaa <gunel.sultanova@outlook.com>
2024-04-03 14:41:34 +0400

    state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -4,16 +4,18 @@ from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
-class test_state(test_basemodel):
-    """ """
-
+class TestState(test_basemodel):
+    """Represents the tests for the State model."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        """Tests the type of name."""
+        new = self.value(name="California")
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )

7232dfa69d5cce88da573576d6b25e43817a34c3
heydarov93 <heyderov.yashar@mail.ru>
2024-04-03 10:02:19 +0400

    double quote added to value of the secondary argument

--- models/place.py
+++ models/place.py
@@ -6,23 +6,6 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = "places"
-
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
-
-
     place_amenity = Table("place_amenity", Base.metadata,
                           Column(
                                  "place_id",
@@ -40,6 +23,22 @@ class Place(BaseModel, Base):
                                  ))
 
 
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
+
     # for DBStorage
     # one to many
     if getenv("HBNB_TYPE_STORAGE") == "db":
@@ -48,7 +47,7 @@ class Place(BaseModel, Base):
                                 backref="place",
                                 cascade="all, delete"
                                 )
-        amenities = relationship("Amenity", secondary=place_amenity,
+        amenities = relationship("Amenity", secondary="place_amenity",
                                  viewonly=False)
     else:
         @property

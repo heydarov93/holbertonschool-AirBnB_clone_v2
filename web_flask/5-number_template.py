c51fdf5e89284ceb2a9580334ea902b4d0938c28
heydarov93 <yashar.heyderov@mail.ru>
2024-04-14 12:30:20 +0400

    Display html template using dynamic url

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """Home page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def to_hbnb():
+    """Hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def to_ctext(text):
+    """C/text page"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def to_python_text(text="is cool"):
+    """Python/text page"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def to_number(n):
+    """Number page displays dynamic int"""
+    return "{:d} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def to_number_template(n):
+    """Displays number html template"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

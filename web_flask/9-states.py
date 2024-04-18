187e1db57431dc5041693314d5414a87499bcf9f
heydarov93 <heyderov.yashar@mail.ru>
2024-04-18 12:57:30 +0400

    Flask application that fetchs states and renders html page with either all states or just on state selected by id

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states")
+@app.route("/states/<uuid:id>")
+def to_states(id):
+    """
+    Fetchs states and cities by id
+    then renders html page with that data
+    """
+    states_tmp = storage.all(State)
+    state_by_id = None
+    states = []
+
+    if id:
+        state = states_tmp[f"State.{id}"]
+        state_by_id = state
+    else:
+        for obj in states_tmp.values():
+            states.append(obj)
+
+    return render_template(
+                            "8-cities_by_states.html",
+                            states=states,
+                            single_state=state_by_id
+                          )
+
+
+@app.teardown_appcontext
+def close(exc):
+    """Closes session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
